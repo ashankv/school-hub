84e60def217230599124f33eb0ae70454cb63b06
Ashank Verma <Ashank Verma>
2015-11-07 17:04:20 -0800

    Added Toolbar

--- app/src/main/java/cf/schoolhub/schoolhub/MainActivity.java
+++ app/src/main/java/cf/schoolhub/schoolhub/MainActivity.java
@@ -2,17 +2,39 @@ package cf.schoolhub.schoolhub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
